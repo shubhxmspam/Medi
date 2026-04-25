5113817474578f0d39fc718dd517635e157200f1
sumitkumai <kumaisumit688@gmail.com>
2026-04-25 12:18:21 +0000

    day17

--- client/src/app/mediconnect/components/patientsample/patientsample.component.ts
+++ client/src/app/mediconnect/components/patientsample/patientsample.component.ts
@@ -1,4 +1,17 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { Patient } from "../../models/Patient";
 
+@Component({
+    selector: 'app-patientsample',
+    standalone: true,
+    templateUrl: './patientsample.component.html',
+    styleUrls: ['./patientsample.component.scss'],
+    imports: [CommonModule]
+})  
 export class PatientSampleComponent {
-  
+    patient: Patient = new Patient(1, 'John Doe', new Date('1990-01-01'), '1234567890', 'john@example.com', '123 Main Street, Cityville');
+    logPatientAttribute(){
+        this.patient.logAttributes();
+    }
 }
