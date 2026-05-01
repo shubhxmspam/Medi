213fd054356b96a50f0a8d510835533dd2aad4ce
sumitkumai <kumaisumit688@gmail.com>
2026-05-01 15:01:43 +0000

    day20

--- client/src/app/mediconnect/components/appointment/appointment.component.ts
+++ client/src/app/mediconnect/components/appointment/appointment.component.ts
@@ -1,4 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-export class AppointmentComponent {
+@Component({
+    selector: 'app-appointment',
+    templateUrl: './appointment.component.html',
+    styleUrls: ['./appointment.component.scss']
+})
+export class AppointmentCreateComponent implements OnInit {
+    appointmentForm!: FormGroup;
+    successMessage: string | null = null;
+    errorMessage: string | null = null;
 
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.appointmentForm = this.formBuilder.group({
+            appointmentId: [null, [Validators.required, Validators.min(1)]],
+            patientId: [null, [Validators.required, Validators.min(1)]],
+            clinicId: [null, [Validators.required, Validators.min(1)]],
+            appointmentDate: ['', [Validators.required]],
+            status: ['', [Validators.required]],
+            purpose: ['', [Validators.required, Validators.minLength(5)]]
+        });
+    }
+
+    onSubmit(): void {
+        if (this.appointmentForm.valid) {
+            this.successMessage = 'Appointment has been successfully created!';
+            this.errorMessage = null;
+            console.log('Appointment Created: ', this.appointmentForm.value);
+            this.resetForm();
+        } else {
+            this.errorMessage = 'Please fill out all required fields correctly.';
+            this.successMessage = null;
+        }
+    }
+
+    resetForm(): void {
+        this.appointmentForm.reset({
+            appointmentId: null,
+            patientId: null,
+            clinicId: null,
+            appointmentDate: '',
+            status: '',
+            purpose: ''
+        });
+    }
 }
